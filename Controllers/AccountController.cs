e97068f38c07435a0baae12b96763b7840401814
TiagoJVAlves <tiagojvalves@gmail.com>
2022-01-09 18:46:48 +0000

    Added LoginViewModel

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using IdentityExp2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExp2.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
